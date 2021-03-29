73972df82145aaff2c35d82e1859c292c1c9c459
saydash <smiftakhov@nes.ru>
2021-03-29 22:46:10 +0300

    reqs and install-start scripts added

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/maftukh/test_delivery_api.git
-cd test_delivery_api
 sudo apt install virtualenv gunicorn
 virtualenv -p python3 venv
 source venv/bin/activate
+cd test_delivery_api
 pip install -r requirements.txt
